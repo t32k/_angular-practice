947300d5c812b76d930685c7c933c3107874464f
t32k <ijok.ijok@gmail.com>
2015-08-03 17:12:58 +0900

    Add custom trigger

--- js/app.js
+++ js/app.js
@@ -1,6 +1,22 @@
-angular.module('ui.bootstrap.demo', ['ui.bootstrap']);
-angular.module('ui.bootstrap.demo').controller('TooltipDemoCtrl', function ($scope, $sce) {
-    $scope.dynamicTooltip = 'Hello, World!';
-    $scope.dynamicTooltipText = 'dynamic';
-    $scope.htmlTooltip = $sce.trustAsHtml('I\'ve been made <b>bold</b>!');
-});
+'use strict';
+
+angular.module('myApp',['ui.bootstrap'])
+    .config(['$tooltipProvider', function($tooltipProvider){
+        $tooltipProvider.setTriggers({'customEvent': 'customEvent'});
+    }]);
+
+angular.module('myApp').controller('myController', ['$scope','$timeout', '$sce',
+    function($scope, $timeout, $sce) {
+        $scope.show = function() {
+            $timeout(function() {
+                $('.tt').trigger('customEvent');
+            }, 0);
+        };
+        $scope.hide = function() {
+            $timeout(function() {
+                $('.tt').trigger('customEvent');
+            }, 1000);
+        };
+
+        $scope.htmlTooltip = $sce.trustAsHtml('<div class="tt" tooltip-trigger="customEvent" ng-mouseenter="show()">まだまだ入社したばかりで教えていただくことばかりでありますが、1日でも早く<a href="#">事業に貢献</a>できるように頑張って参ります。歴史に名を刻めるくらいのプロダクトを作りたいです。どうぞよろしくお願いします！</a>');
+    }]);
