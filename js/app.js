850c8f1011338dbdd361dd14148e4009cad62491
t32k <ijok.ijok@gmail.com>
2015-08-03 10:15:53 +0900

    Practice tooltip

---
+++ js/app.js
@@ -0,0 +1,6 @@
+angular.module('ui.bootstrap.demo', ['ui.bootstrap']);
+angular.module('ui.bootstrap.demo').controller('TooltipDemoCtrl', function ($scope, $sce) {
+    $scope.dynamicTooltip = 'Hello, World!';
+    $scope.dynamicTooltipText = 'dynamic';
+    $scope.htmlTooltip = $sce.trustAsHtml('I\'ve been made <b>bold</b>!');
+});
